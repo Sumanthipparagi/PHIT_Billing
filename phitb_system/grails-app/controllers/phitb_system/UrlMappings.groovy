a40f4088a261d998facf7acaa5c335b51598fbe5
Arjun K R <arjunarjunkr@hotmail.com>
2022-01-04 09:39:40 +0530

    test commit

--- phitb_system/grails-app/controllers/phitb_system/UrlMappings.groovy
+++ phitb_system/grails-app/controllers/phitb_system/UrlMappings.groovy
@@ -3,15 +3,16 @@ package phitb_system
 class UrlMappings {
 
     static mappings = {
-        delete "/$controller/$id(.$format)?"(action:"delete")
-        get "/$controller(.$format)?"(action:"index", version: "1.0")
-        get "/$controller/search(.$format)?"(action:"search")
-        get "/$controller/$id(.$format)?"(action:"show")
-        post "/$controller(.$format)?"(action:"save", version: "1.0", method:"POST")
-        put "/$controller/$id(.$format)?"(action:"update")
+        delete "/$controller/$id(.$format)?"(action: "delete")
+        get "/$controller(.$format)?"(action: "index", version: "1.0")
+        get "/$controller/search(.$format)?"(action: "search")
+        get "/$controller/$id(.$format)?"(action: "show")
+        post "/$controller(.$format)?"(action: "save", version: "1.0", method: "POST")
+        put "/$controller/$id(.$format)?"(action: "update")
         //patch "/$controller/$id(.$format)?"(action:"patch")
 
-        "/"(controller: 'application', action:'index')
+
+        "/"(controller: 'application', action: 'index')
         "500"(view: '/error')
         "404"(view: '/notFound')
     }
