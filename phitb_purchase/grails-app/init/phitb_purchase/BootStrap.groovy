e9135c8e942b0d876bb0cb25c8a28e8c97da68ff
Arjun K R <arjunarjunkr@hotmail.com>
2022-01-13 19:00:49 +0530

    date and time JSON Marshaller added

--- phitb_purchase/grails-app/init/phitb_purchase/BootStrap.groovy
+++ phitb_purchase/grails-app/init/phitb_purchase/BootStrap.groovy
@@ -1,8 +1,19 @@
 package phitb_purchase
 
+import grails.converters.JSON
+
+import java.sql.Time
+
 class BootStrap {
 
     def init = { servletContext ->
+        JSON.registerObjectMarshaller(Date) {
+            return it?.format("dd/MM/yyyy HH:mm:ss")
+        }
+
+        JSON.registerObjectMarshaller(Time) {
+            return it?.format("HH:mm:ss")
+        }
     }
     def destroy = {
     }
