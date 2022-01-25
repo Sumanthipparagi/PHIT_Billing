035f4953177261b313428aa30f79d58947ca8d57
sumant HS <sumanthhs@capulustech.in>
2022-01-25 14:20:22 +0530

    Domains

---
+++ phitb_entity/grails-app/controllers/phitb_entity/EntityTypeMasterController.groovy
@@ -0,0 +1,168 @@
+package phitb_entity
+
+
+import grails.rest.*
+import grails.converters.*
+import grails.web.servlet.mvc.GrailsParameterMap
+import org.grails.web.json.JSONObject
+import phitb_entity.Exception.BadRequestException
+import phitb_entity.Exception.ResourceNotFoundException
+
+class EntityTypeMasterController {
+	static responseFormats = ['json', 'xml']
+
+    static allowedMethods = [index: "GET", show: "GET", save: "POST", update: "PUT", delete: "DELETE", dataTable: "GET"]
+
+    EntityTypeMasterService entityTypeMasterService
+    /**
+     * Gets all entity type master
+     * @param query
+     * @param offset
+     * @param limit
+     * @return list of entity type master
+     */
+    def index() {
+
+        try {
+            respond entityTypeMasterService.getAll(params.limit, params.offset, params.query)
+        }
+        catch (Exception ex) {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+        }
+    }
+
+    /**
+     * Get requested entity type master
+     * @param id
+     * @return get requested entity type master
+     */
+    def show() {
+        try {
+            String id = params.id
+            if (id) {
+                respond entityTypeMasterService.get(id)
+            }
+        }
+        catch (ResourceNotFoundException ex)
+        {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+            response.status = 404
+        }
+        catch (BadRequestException ex)
+        {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+            response.status = 400
+        }
+        catch (Exception ex) {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+        }
+    }
+
+
+    /**
+     * Save new entity type master
+     * @param entity type master
+     * @return saved entity type master
+     */
+    def save() {
+        try {
+            JSONObject jsonObject = JSON.parse(request.reader.text) as JSONObject
+            respond entityTypeMasterService.save(jsonObject)
+        }
+        catch (ResourceNotFoundException ex)
+        {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+            response.status = 404
+        }
+        catch (BadRequestException ex)
+        {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+            response.status = 400
+        }
+        catch (Exception ex) {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+        }
+    }
+
+    /**
+     * Update existing entity type master
+     * @param id
+     * @param entity type master
+     * @return updated entity type master
+     */
+    def update() {
+        try {
+            String id = params.id
+            JSONObject jsonObject = JSON.parse(request.reader.text) as JSONObject
+            respond entityTypeMasterService.update(jsonObject,id)
+        }
+        catch (ResourceNotFoundException ex)
+        {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+            response.status = 404
+        }
+        catch (BadRequestException ex)
+        {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+            response.status = 400
+        }
+        catch (Exception ex) {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+        }
+    }
+
+
+    /**
+     * Delete selected account register
+     * @param id
+     * @return returns status code 200
+     */
+    def delete() {
+        try {
+            String id = params.id
+            entityTypeMasterService.delete(id)
+            response.status = 200
+        }
+        catch (ResourceNotFoundException ex)
+        {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+            response.status = 404
+        }
+        catch (BadRequestException ex)
+        {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+            response.status = 400
+        }
+        catch (Exception ex) {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+        }
+    }
+
+
+    /**
+     * Gets all bank register in datatables format
+     * @return list of bank register
+     */
+    def dataTable() {
+        try {
+            String start = params.start
+            String length = params.length
+            GrailsParameterMap parameterMap = getParams()
+            JSONObject paramsJsonObject = new JSONObject(parameterMap.params)
+            respond entityTypeMasterService.dataTables(paramsJsonObject, start, length)
+        }
+        catch (ResourceNotFoundException ex)
+        {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+            response.status = 404
+        }
+        catch (BadRequestException ex)
+        {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+            response.status = 400
+        }
+        catch (Exception ex) {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+        }
+    }
+}
