36b67edb7596aba4b9019d11a5a68a53da3a73a5
Arjun K R <arjunarjunkr@hotmail.com>
2022-01-27 13:10:06 +0530

    phitb_sales domains started

---
+++ phitb_sales/grails-app/domain/phitb_sales/ReasonMaster.groovy
@@ -0,0 +1,30 @@
+package phitb_sales
+
+import gorm.logical.delete.LogicalDelete
+
+class ReasonMaster implements Serializable, LogicalDelete<ReasonMaster> {
+
+    String reasonName
+    String reasonCode
+    Date dateCreated
+    Date lastUpdated
+
+    static constraints = {
+    }
+
+    boolean isUpdatable
+    static transients = ['isUpdatable']
+    def beforeUpdate()
+    {
+
+        if (!this.isUpdatable)
+        {
+            System.out.println("ReasonMaster Domain update Prevented " + new Date().toString() + " ,id: " + this.id)
+            return false
+        }
+        else
+        {
+            System.out.println("ReasonMaster domain Updated " + new Date().toString() + " ,id: " + this.id)
+        }
+    }
+}
---
+++ phitb_sales/src/test/groovy/phitb_sales/ReasonMasterSpec.groovy
@@ -0,0 +1,18 @@
+package phitb_sales
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class ReasonMasterSpec extends Specification implements DomainUnitTest<ReasonMaster> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
