af0942e358e81dd07bcc620ccacae5ac5ccb2b95
sumant HS <sumanthhs@capulustech.in>
2022-03-08 19:02:48 +0530

    tempstockbook

--- phitb_ui/grails-app/services/phitb_ui/InventoryService.groovy
+++ phitb_ui/grails-app/services/phitb_ui/InventoryService.groovy
@@ -37,7 +37,6 @@ class InventoryService {
         WebTarget target = client.target(new Links().API_GATEWAY);
         GrailsHttpSession session = WebUtils.retrieveGrailsWebRequest().session
         try {
-
             Response apiResponse = target
                     .path(new Links().GET_TEMP_STOCK_PRODUCT_BATCH + "/product/" + id + "/batch/"+ batch)
                     .request(MediaType.APPLICATION_JSON_TYPE)
@@ -51,24 +50,24 @@ class InventoryService {
         }
     }
 
-    def getTempStocksOfEntity(String id) {
-        Client client = ClientBuilder.newClient();
-        WebTarget target = client.target(new Links().API_GATEWAY);
-        GrailsHttpSession session = WebUtils.retrieveGrailsWebRequest().session
-        try {
-
-            Response apiResponse = target
-                    .path(new Links().GET_TEMP_STOCK_PRODUCT_ENTITY + "/" + id)
-                    .request(MediaType.APPLICATION_JSON_TYPE)
-                    .get()
-
-            return apiResponse
-        }
-        catch (Exception ex) {
-            System.err.println('Service :ProductService , action :  getBatchesOfProduct  , Ex:' + ex)
-            log.error('Service :ProductService , action :  getBatchesOfProduct  , Ex:' + ex)
-        }
-    }
+//    def getTempStocksOfEntity(String id) {
+//        Client client = ClientBuilder.newClient();
+//        WebTarget target = client.target(new Links().API_GATEWAY);
+//        GrailsHttpSession session = WebUtils.retrieveGrailsWebRequest().session
+//        try {
+//
+//            Response apiResponse = target
+//                    .path(new Links().GET_TEMP_STOCK_PRODUCT_ENTITY + "/" + id)
+//                    .request(MediaType.APPLICATION_JSON_TYPE)
+//                    .get()
+//
+//            return apiResponse
+//        }
+//        catch (Exception ex) {
+//            System.err.println('Service :ProductService , action :  getBatchesOfProduct  , Ex:' + ex)
+//            log.error('Service :ProductService , action :  getBatchesOfProduct  , Ex:' + ex)
+//        }
+//    }
 
 
 }
