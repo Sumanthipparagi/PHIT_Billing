a0fc6a6872ff84935e3152057e2466dfb1e94116
Arjun K R <arjunarjunkr@hotmail.com>
2022-03-12 16:07:52 +0530

    Username check added while inserting

--- phitb_entity/grails-app/services/phitb_entity/UserRegisterService.groovy
+++ phitb_entity/grails-app/services/phitb_entity/UserRegisterService.groovy
@@ -77,58 +77,66 @@ class UserRegisterService {
     }
 
     UserRegister save(JSONObject jsonObject) {
-        UserRegister userRegister = new UserRegister()
-        userRegister.userName = jsonObject.get("userName").toString()
-        userRegister.mobileNumber = jsonObject.get("mobileNumber").toString()
-        userRegister.contactNumber = jsonObject.get("contactNumber").toString()
-        userRegister.aadharId = jsonObject.get("aadharId").toString()
-        userRegister.email = jsonObject.get("email").toString()
-        userRegister.photo = jsonObject.get("photo").toString()
-        userRegister.photo = userRegister.getPhoto()
-        userRegister.nationality = jsonObject.get("nationality").toString()
-        userRegister.address = jsonObject.get("address").toString()
-        userRegister.referenceRelation = jsonObject.get("referenceRelation").toString()
-        userRegister.pincode = jsonObject.get("pincode").toString()
-        userRegister.bankAccount = jsonObject.get("bankAccount").toString()
-        userRegister.permissions = jsonObject.get("permissions").toString()
-        userRegister.assignedHolidays = jsonObject.get("assignedHolidays").toString()
-        userRegister.specialization = jsonObject.get("specialization").toString()
-        userRegister.licenceNumber = jsonObject.get("licenceNumber").toString()
-        userRegister.reportTo = Long.parseLong(jsonObject.get("reportTo").toString())
-        userRegister.genderId = Long.parseLong(jsonObject.get("genderId").toString())
-        userRegister.countryId = Long.parseLong(jsonObject.get("countryId").toString())
-        userRegister.stateId = Long.parseLong(jsonObject.get("stateId").toString())
-        userRegister.cityId = Long.parseLong(jsonObject.get("cityId").toString())
-        userRegister.bankId = Long.parseLong(jsonObject.get("bankId").toString())
-        userRegister.zoneId = Long.parseLong(jsonObject.get("zoneId").toString())
-        userRegister.divisionId = Long.parseLong(jsonObject.get("divisionId").toString())
-        userRegister.referredBy = Long.parseLong(jsonObject.get("referredBy").toString())
-        userRegister.status = Long.parseLong(jsonObject.get("stateId").toString())
-        userRegister.syncStatus = Long.parseLong(jsonObject.get("syncStatus").toString())
-        userRegister.paymentModeId = Long.parseLong(jsonObject.get("paymentModeId").toString())
-        userRegister.approvedSalary = Double.parseDouble(jsonObject.get("approvedSalary").toString())
-        userRegister.designationSalary = Double.parseDouble(jsonObject.get("designationSalary").toString())
-        userRegister.joiningDate = sdf.parse(jsonObject.get("joiningDate").toString())
-        userRegister.dob = sdf.parse(jsonObject.get("dob").toString())
-        userRegister.anniversaryDate = sdf.parse(jsonObject.get("anniversaryDate").toString())
-        userRegister.lastLoginDate = sdf.parse(jsonObject.get("lastLoginDate").toString())
-        userRegister.lastPaidDate = sdf.parse(jsonObject.get("lastPaidDate").toString())
-        userRegister.department = DepartmentMaster.findById(Long.parseLong(jsonObject.get("department").toString()))
-        userRegister.account = AccountRegister.findById(Long.parseLong(jsonObject.get("account").toString()))
-        userRegister.role = RoleMaster.findById(Long.parseLong(jsonObject.get("role").toString()))
-        userRegister.entityType = EntityTypeMaster.findById(Long.parseLong(jsonObject.get("entityType").toString()))
-        userRegister.entity = EntityRegister.findById(Long.parseLong(jsonObject.get("entity").toString()))
-        userRegister.createdUser = Long.parseLong(jsonObject.get("createdUser").toString())
-        userRegister.modifiedUser = Long.parseLong(jsonObject.get("modifiedUser").toString())
-        userRegister.save(flush: true)
-        if (!userRegister.hasErrors()) {
-            AuthRegister authRegister = new AuthRegister()
-            authRegister.user = userRegister
-            authRegister.username = userRegister.userName
-            authRegister.password = new AuthRegisterService().hashPassword(jsonObject.get("password").toString())
-            authRegister.save(flush: true)
-            return userRegister
-        } else {
+        String username = jsonObject.get("userName").toString()
+        if(!UserRegister.findByUserName(username)) {
+            UserRegister userRegister = new UserRegister()
+            userRegister.userName = username
+            userRegister.mobileNumber = jsonObject.get("mobileNumber").toString()
+            userRegister.contactNumber = jsonObject.get("contactNumber").toString()
+            userRegister.aadharId = jsonObject.get("aadharId").toString()
+            userRegister.email = jsonObject.get("email").toString()
+            userRegister.photo = jsonObject.get("photo").toString()
+            userRegister.photo = userRegister.getPhoto()
+            userRegister.nationality = jsonObject.get("nationality").toString()
+            userRegister.address = jsonObject.get("address").toString()
+            userRegister.referenceRelation = jsonObject.get("referenceRelation").toString()
+            userRegister.pincode = jsonObject.get("pincode").toString()
+            userRegister.bankAccount = jsonObject.get("bankAccount").toString()
+            userRegister.permissions = jsonObject.get("permissions").toString()
+            userRegister.assignedHolidays = jsonObject.get("assignedHolidays").toString()
+            userRegister.specialization = jsonObject.get("specialization").toString()
+            userRegister.licenceNumber = jsonObject.get("licenceNumber").toString()
+            userRegister.reportTo = Long.parseLong(jsonObject.get("reportTo").toString())
+            userRegister.genderId = Long.parseLong(jsonObject.get("genderId").toString())
+            userRegister.countryId = Long.parseLong(jsonObject.get("countryId").toString())
+            userRegister.stateId = Long.parseLong(jsonObject.get("stateId").toString())
+            userRegister.cityId = Long.parseLong(jsonObject.get("cityId").toString())
+            userRegister.bankId = Long.parseLong(jsonObject.get("bankId").toString())
+            userRegister.zoneId = Long.parseLong(jsonObject.get("zoneId").toString())
+            userRegister.divisionId = Long.parseLong(jsonObject.get("divisionId").toString())
+            userRegister.referredBy = Long.parseLong(jsonObject.get("referredBy").toString())
+            userRegister.status = Long.parseLong(jsonObject.get("stateId").toString())
+            userRegister.syncStatus = Long.parseLong(jsonObject.get("syncStatus").toString())
+            userRegister.paymentModeId = Long.parseLong(jsonObject.get("paymentModeId").toString())
+            userRegister.approvedSalary = Double.parseDouble(jsonObject.get("approvedSalary").toString())
+            userRegister.designationSalary = Double.parseDouble(jsonObject.get("designationSalary").toString())
+            userRegister.joiningDate = sdf.parse(jsonObject.get("joiningDate").toString())
+            userRegister.dob = sdf.parse(jsonObject.get("dob").toString())
+            userRegister.anniversaryDate = sdf.parse(jsonObject.get("anniversaryDate").toString())
+            userRegister.lastLoginDate = sdf.parse(jsonObject.get("lastLoginDate").toString())
+            userRegister.lastPaidDate = sdf.parse(jsonObject.get("lastPaidDate").toString())
+            userRegister.department = DepartmentMaster.findById(Long.parseLong(jsonObject.get("department").toString()))
+            userRegister.account = AccountRegister.findById(Long.parseLong(jsonObject.get("account").toString()))
+            userRegister.role = RoleMaster.findById(Long.parseLong(jsonObject.get("role").toString()))
+            userRegister.entityType = EntityTypeMaster.findById(Long.parseLong(jsonObject.get("entityType").toString()))
+            userRegister.entity = EntityRegister.findById(Long.parseLong(jsonObject.get("entity").toString()))
+            userRegister.createdUser = Long.parseLong(jsonObject.get("createdUser").toString())
+            userRegister.modifiedUser = Long.parseLong(jsonObject.get("modifiedUser").toString())
+            userRegister.save(flush: true)
+            if (!userRegister.hasErrors()) {
+                AuthRegister authRegister = new AuthRegister()
+                authRegister.user = userRegister
+                authRegister.username = userRegister.userName
+                authRegister.password = new AuthRegisterService().hashPassword(jsonObject.get("password").toString())
+                authRegister.save(flush: true)
+                return userRegister
+            } else {
+                throw new BadRequestException()
+            }
+        }
+        else
+        {
+            println("User already exists")
             throw new BadRequestException()
         }
     }
@@ -137,7 +145,7 @@ class UserRegisterService {
         UserRegister userRegister = UserRegister.findById(Long.parseLong(id))
         if (userRegister) {
             userRegister.isUpdatable = true
-            userRegister.userName = jsonObject.get("userName").toString()
+            //userRegister.userName = jsonObject.get("userName").toString()
             userRegister.mobileNumber = jsonObject.get("mobileNumber").toString()
             userRegister.contactNumber = jsonObject.get("contactNumber").toString()
             userRegister.aadharId = jsonObject.get("aadharId").toString()
