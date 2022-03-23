02513ae5bd78337df822f332783cf328d4070738
sumant HS <sumanthhs@capulustech.in>
2022-03-23 10:31:34 +0530

    Sale invoice

--- phitb_ui/grails-app/controllers/phitb_ui/sales/SaleEntryController.groovy
+++ phitb_ui/grails-app/controllers/phitb_ui/sales/SaleEntryController.groovy
@@ -261,8 +261,6 @@ class SaleEntryController {
         JSONObject city = new SystemService().getCityById(entity.get('cityId').toString())
         JSONObject custcity = new SystemService().getCityById(customer.get('cityId').toString())
         JSONArray termsConditions = new EntityService().getTermsContionsByEntity(session.getAttribute("entityId").toString())
-        /*JSONObject term = new JSONObject()
-        term.put("termsConditions", termsConditions);*/
         saleProductDetails.each{
             def apiResponse = new SalesService().getRequestWithId(it.productId.toString(),new Links().PRODUCT_REGISTER_SHOW)
             it.put("productId",JSON.parse(apiResponse.readEntity(String.class)) as JSONObject)
@@ -279,11 +277,18 @@ class SaleEntryController {
         else {
             invoiceNumber = "S/"+month+year+"/"+series.seriesCode+"/"+saleBillDetail.id
         }
-        render(view: "/sales/sale-invoice", model: [saleBillDetail: saleBillDetail,
+
+        def totalcgst = saleProductDetails.cgstAmount.sum()
+        def totalsgst = saleProductDetails.sgstAmount.sum()
+        def totaligst = saleProductDetails.igstAmount.sum()
+        def totaldiscount = saleProductDetails.discount.sum()
+       render(view: "/sales/sale-invoice", model: [saleBillDetail: saleBillDetail,
                                                     saleProductDetails:saleProductDetails,
                                                     series:series, entity:entity,customer:customer,city:city,
-                                                    total:saleProductDetails?.amount?.sum(),custcity:custcity,
-                                                    invoiceNumber:invoiceNumber,termsConditions:termsConditions])
+                                                    total:saleProductDetails.amount.sum(),custcity:custcity,
+                                                    invoiceNumber:invoiceNumber,termsConditions:termsConditions,
+                                                     totalcgst:totalcgst,totalsgst:totalsgst,totaligst:totaligst,
+                                                   totaldiscount:totaldiscount])
     }
 
     def show()
