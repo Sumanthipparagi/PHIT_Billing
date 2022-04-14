166a29f41f852a22c51852bf520289e8ddb71f00
Arjun K R <arjunarjunkr@hotmail.com>
2022-04-14 13:45:46 +0530

    reports started

---
+++ phitb_ui/grails-app/controllers/phitb_ui/reports/SalesReportController.groovy
@@ -0,0 +1,13 @@
+package phitb_ui.reports
+
+class SalesReportController {
+
+    def index() { }
+
+    def salesReport()
+    {
+       /* String entityId = session.getAttribute("entityId")
+        String cityId = params.cityId
+        String daterange = params.daterange*/
+    }
+}
