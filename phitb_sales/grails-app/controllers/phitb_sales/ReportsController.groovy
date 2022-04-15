1c24ad861aae51bdd5d2102e344742b97387c78b
Arjun K R <arjunarjunkr@hotmail.com>
2022-04-15 10:10:53 +0530

    Reports API updated

--- phitb_sales/grails-app/controllers/phitb_sales/ReportsController.groovy
+++ phitb_sales/grails-app/controllers/phitb_sales/ReportsController.groovy
@@ -9,34 +9,40 @@ import org.grails.web.json.JSONObject
 import java.text.SimpleDateFormat
 
 class ReportsController {
-	static responseFormats = ['json', 'xml']
-	
-    def getCustomerWiseBillDetails() {
-        JSONObject jsonObject = new JSONObject(request.reader.text)
-        String entityId = jsonObject.get("entityId")
-        String financialYear = jsonObject.get("financialYear")
-        String daterange = jsonObject.get("dateRange")
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy")
-        Date fromDate = sdf.parse(daterange.split("-")[0].trim())
-        Date toDate = sdf.parse(daterange.split("-")[1].trim())
-        JSONObject customerBills = new JSONObject()
-        JSONArray bills = new JSONArray()
-        ArrayList<SaleBillDetails> saleBillDetails = SaleBillDetails.findAllByEntityIdAndFinancialYearAndOrderDateBetweenAndBillStatus(Long.parseLong(entityId),financialYear, fromDate, toDate, "ACTIVE")
-        for (SaleBillDetails saleBillDetail : saleBillDetails) {
-            if(customerBills.containsKey(saleBillDetail.customerId))
-            {
-                bills = customerBills.get(saleBillDetail.customerId)
-                bills.add(saleBillDetail)
-                customerBills.put(saleBillDetail.customerId, bills)
-            }
-            else
-            {
-                bills = new JSONArray()
-                bills.add(saleBillDetail)
-                customerBills.put(saleBillDetail.customerId, bills)
-            }
-        }
+    static responseFormats = ['json', 'xml']
+    static allowedMethods = [getCustomerWiseBillDetails: ["GET", "POST"]]
 
-        respond customerBills
+    def getCustomerWiseBillDetails() {
+        try {
+            JSONObject jsonObject = new JSONObject(request.reader.text)
+            String entityId = jsonObject.get("entityId")
+            String financialYear = jsonObject.get("financialYear")
+            String daterange = jsonObject.get("dateRange")
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy")
+            Date fromDate = sdf.parse(daterange.split("-")[0].trim())
+            Date toDate = sdf.parse(daterange.split("-")[1].trim())
+            JSONObject customerBills = new JSONObject()
+            JSONArray bills = new JSONArray()
+            ArrayList<SaleBillDetails> saleBillDetails = SaleBillDetails.findAllByEntityIdAndFinancialYearAndOrderDateBetweenAndBillStatus(Long.parseLong(entityId), financialYear, fromDate, toDate, "ACTIVE")
+            for (SaleBillDetails saleBillDetail : saleBillDetails) {
+                def saleBillJson = new JSONObject((saleBillDetail as JSON).toString())
+                ArrayList<SaleProductDetails> saleProductDetails = SaleProductDetails.findAllByBillId(saleBillDetail.id)
+                saleBillJson.put("products", saleProductDetails)
+                if (customerBills.containsKey(saleBillJson.customerId)) {
+                    bills = customerBills.get(saleBillJson.customerId) as JSONArray
+                    bills.add(saleBillJson)
+                    customerBills.put(saleBillJson.customerId, bills)
+                } else {
+                    bills = new JSONArray()
+                    bills.add(saleBillJson)
+                    customerBills.put(saleBillJson.customerId, bills)
+                }
+            }
+
+            render customerBills as JSON
+        }
+        catch (Exception ex) {
+            println(ex.stackTrace)
+        }
     }
 }
