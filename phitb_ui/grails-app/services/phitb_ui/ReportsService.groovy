ee426402efb707e052ce7067d65102f4c96670e2
Arjun K R <arjunarjunkr@hotmail.com>
2022-04-15 17:27:56 +0530

    bug fixes

--- phitb_ui/grails-app/services/phitb_ui/ReportsService.groovy
+++ phitb_ui/grails-app/services/phitb_ui/ReportsService.groovy
@@ -19,8 +19,8 @@ class ReportsService {
         jsonObject.put("dateRange", dateRange)
         jsonObject.put("financialYear", financialYear)
         Client client = ClientBuilder.newClient()
-        WebTarget target = client.target(new Links().API_GATEWAY)
-        //WebTarget target = client.target("http://localhost:8083")
+        //WebTarget target = client.target(new Links().API_GATEWAY)
+        WebTarget target = client.target("http://localhost:8083")
         try
         {
             Response apiResponse = target
