03972b1f7daf8067bb7abd25ce8f29a10b84a4ff
sumant HS <sumanthhs@capulustech.in>
2022-04-18 12:04:09 +0530

    Bug fixes

--- phitb_accounts/grails-app/controllers/phitb_accounts/ReceiptDetailLogController.groovy
+++ phitb_accounts/grails-app/controllers/phitb_accounts/ReceiptDetailLogController.groovy
@@ -3,9 +3,161 @@ package phitb_accounts
 
 import grails.rest.*
 import grails.converters.*
+import grails.web.servlet.mvc.GrailsParameterMap
+import org.grails.web.json.JSONObject
+import phitb_accounts.Exception.BadRequestException
+import phitb_accounts.Exception.ResourceNotFoundException
 
 class ReceiptDetailLogController {
 	static responseFormats = ['json', 'xml']
-	
-    def index() { }
+
+    ReceiptDetailLogService receiptDetailLogService
+    /**
+     * Gets all bill payment log
+     * @param query
+     * @param offset
+     * @param limit
+     * @return list of bill payment log
+     */
+    def index() {
+
+        try {
+            respond receiptDetailLogService.getAll(params.limit, params.offset, params.query)
+        }
+        catch (Exception ex) {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+        }
+    }
+
+    /**
+     * Get requested bill payment log
+     * @param id
+     * @return get requested bill payment log
+     */
+    def show() {
+        try {
+            String id = params.id
+            if (id) {
+                respond receiptDetailLogService.get(id)
+            }
+        }
+        catch (ResourceNotFoundException ex)
+        {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+            response.status = 404
+        }
+        catch (BadRequestException ex)
+        {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+            response.status = 400
+        }
+        catch (Exception ex) {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+        }
+    }
+
+    /**
+     * Save new bill payment log
+     * @param bill payment log
+     * @return saved bill payment log
+     */
+    def save() {
+        try {
+            JSONObject jsonObject = JSON.parse(request.reader.text) as JSONObject
+            respond receiptDetailLogService.save(jsonObject)
+        }
+        catch (ResourceNotFoundException ex)
+        {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+            response.status = 404
+        }
+        catch (BadRequestException ex)
+        {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+            response.status = 400
+        }
+        catch (Exception ex) {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+        }
+    }
+
+    /**
+     * Update existing bill payment log
+     * @param id
+     * @param bill payment log
+     * @return updated bill payment log
+     */
+    def update() {
+        try {
+            String id = params.id
+            JSONObject jsonObject = JSON.parse(request.reader.text) as JSONObject
+            respond receiptDetailLogService.update(jsonObject,id)
+        }
+        catch (ResourceNotFoundException ex)
+        {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+            response.status = 404
+        }
+        catch (BadRequestException ex)
+        {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+            response.status = 400
+        }
+        catch (Exception ex) {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+        }
+    }
+
+    /**
+     * Delete selected bill payment log
+     * @param id
+     * @return returns status code 200
+     */
+    def delete() {
+        try {
+            String id = params.id
+            receiptDetailLogService.delete(id)
+            response.status = 200
+        }
+        catch (ResourceNotFoundException ex)
+        {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+            response.status = 404
+        }
+        catch (BadRequestException ex)
+        {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+            response.status = 400
+        }
+        catch (Exception ex) {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+        }
+    }
+
+    /**
+     * Gets all bill payment log in datatables format
+     * @return list of bill payment log
+     */
+    def dataTable() {
+        try {
+            String start = params.start
+            String length = params.length
+            GrailsParameterMap parameterMap = getParams()
+            JSONObject paramsJsonObject = new JSONObject(parameterMap.params)
+            respond receiptDetailLogService.dataTables(paramsJsonObject, start, length)
+        }
+        catch (ResourceNotFoundException ex)
+        {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+            response.status = 404
+        }
+        catch (BadRequestException ex)
+        {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+            response.status = 400
+        }
+        catch (Exception ex) {
+            System.err.println('Controller :' + controllerName + ', action :' + actionName + ', Ex:' + ex)
+        }
+    }
 }
