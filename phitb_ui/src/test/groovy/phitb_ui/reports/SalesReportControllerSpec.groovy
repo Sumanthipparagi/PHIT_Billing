2920df4005d68674f1ae483fc2d1396c0667fa8b
Arjun K R <arjunkr@capulustech.com>
2022-10-06 19:25:35 +0530

    keystore added to git

---
+++ phitb_ui/src/test/groovy/phitb_ui/reports/SalesReportControllerSpec.groovy
@@ -0,0 +1,18 @@
+package phitb_ui.reports
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class SalesReportControllerSpec extends Specification implements ControllerUnitTest<SalesReportController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
